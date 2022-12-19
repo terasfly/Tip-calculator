f55ec84399c8934b3cf9b167da59e9954ad5b7fb
terasfly <84103159+terasfly@users.noreply.github.com>
2022-12-19 20:24:50 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,37 +1,29 @@
-const btn = document.querySelector(".btn"),
-    tip = document.querySelector(".tip"),
-    total = document.querySelector(".total"),
-    error = document.querySelector(".error");
+const tip = document.querySelector(".tip");
+const total = document.querySelector(".total");
+const error = document.querySelector(".error");
 
-const hideError = () => {
-    setTimeout(() => {
-        error.style.display = "none";
-    }, 3000);
-};
-const calculateTip = () => {
-    const bill = document.querySelector(".bill").value;
-    const rate = document.querySelector(".rate").value;
-
-    if (bill === "" || rate == "") {
-        // console.log("please add  a number");
-        error.style.display = "block";
-        hideError();
-    } else if (isNaN(bill)) {
-        error.textContent = "Please enter a number";
-        error.style.display = "block";
-        hideError();
-    } else {
-        let tipAmt = bill * rate;
-//         console.log(typeof tipAmt);
-        // console.log(bill);
-        // tipAmt = Math.ceil(tipAmt);
-        // console.log(tipAmt);
-        tip.innerHTML = `Tip: $ ${tipAmt}`;
-        console.log(tip);
-
-        // let totalBill = Number(bill) + tipAmt;
-        // total.textContent = `Total Bill: $${totalBill}`;
-    }
-};
+const wallet = document.querySelector(".myWalletAfterPayment");
+const btn = document.querySelector(".btn");
 
 btn.addEventListener("click", calculateTip);
+
+function calculateTip() {
+    const saskaita = document.querySelector(".saskaita").value;
+    const rates = document.querySelector(".rates").value;
+    if (saskaita === "" || rates === "") {
+        error.style.display = "block";
+        error.textContent = "Please enter the number";
+    } else if (isNaN(saskaita)) {
+        error.style.display = "block";
+        error.textContent = "Letters not accepted";
+    } else {
+        let suma = saskaita * rates;
+        suma = Math.ceil(suma);
+        tip.textContent = `Tip amount $${suma}`;
+        let totalus = Number(saskaita) + suma;
+        total.textContent = `Total Bill : $${totalus}`;
+        console.log(suma);
+        let answer = 3456 - totalus;
+        wallet.textContent = `My wallet left : $${answer}`;
+    }
+}
